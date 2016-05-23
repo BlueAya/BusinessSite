b4831c527091fbb0334456243fc45c88a604621a
Angelo Maramara <angelo.maramara@gmail.com>
2016-05-23 13:53:13 -0400

    page.php Loop Implemented

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<!-- Begin Section Container -->
+<section class="row">
+  <div class="twelve columns">
+      <!-- Begin Loop -->
+      <?php
+        if ( have_posts() ) {
+          while ( have_posts() ) {
+            the_post(); ?>
+              <h2><?php the_title(); ?></h2>
+              <?php the_content(); ?>
+            <?php
+            } //end while
+        } //end if
+      ?>
+      <!-- End Loop -->
+
+      </div>
+  </section>
+
+<?php get_footer(); ?>
