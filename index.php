a8095608c87313506332dcec0ca1fc505544e3d8
Angelo Maramara <angelo.maramara@gmail.com>
2016-05-23 13:22:21 -0400

    Extended WordPress Loop With Title and Excerpts

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
           <?php
             if ( have_posts() ) {
               while ( have_posts() ) {
-                the_post();
-              } //end while
+                the_post(); ?>
+                  <h3><?php the_title(); ?></h3>
+                  <?php the_excerpt(); ?>
+                <?php  
+                } //end while
             } //end if
           ?>
           <!-- End Loop -->
