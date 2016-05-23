96a011359ee09925b4c7fc59132a830f450acf0e
Angelo Maramara <angelo.maramara@gmail.com>
2016-05-23 10:18:59 -0400

    Linked and added stylesheets

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
+  <title>Angelo Maramara Blank Template</title>
+  <!-- Links to our Style.css file -->
+  <link rel="stylesheet" type="text/css" href="
+    <?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-  <h1>Blank WordPress Template</h1>
+  <h1>Angelo Maramara Blank WordPress Template</h1>
   <p>This is my WordPress template.</p>
 </body>
 </html>
