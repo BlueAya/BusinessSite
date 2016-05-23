f1db14510585004fbf40b55d5830c63ba0cb1bfb
Angelo Maramara <angelo.maramara@gmail.com>
2016-05-23 10:36:23 -0400

    Skeleton Grid System Classes Added

--- index.php
+++ index.php
@@ -7,7 +7,28 @@
     <?php bloginfo('stylesheet_url'); ?>" />
 </head>
 <body>
-  <h1>Angelo Maramara Blank WordPress Template</h1>
-  <p>This is my WordPress template.</p>
+  <div class="container">
+    <header class="row">
+      <div class="twelve columns">
+        <h1>Angelo Maramara Blank WordPress Template</h1>
+        <p>This is my WordPress template.</p>
+      </div>
+    </header>
+    <section class="row">
+      <div class="twelve columns">
+        <h2>Section Content - cool</h2>
+        <p>This is some cool section content</p>
+      </div>
+    </section>
+    <footer class="row">
+      <div class="twelve columns">
+        <h2>This the Footer</h2>
+        <p>This is some cool footer content</p>
+      </div>
+    </footer>
+  </div>
+
+
+
 </body>
 </html>
