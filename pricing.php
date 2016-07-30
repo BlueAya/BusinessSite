063a72deb837a9386cdadd5f15f7d2237d566294
Angelo Maramara <angelo.maramara@gmail.com>
2016-07-30 12:00:21 -0400

    Add and style Pricing page

--- pricing.php
+++ pricing.php
@@ -10,82 +10,40 @@ Template Name: Pricing
     <div class="container">
         <div class="firstpricingrow row">
             <div class="image col-sm-4">
-                <img class="imghover img-responsive" src="<?php bloginfo('template_directory'); ?>/img/leftmech.png" alt="Sharon" />
-                <h2>Sharon</h2>
-                <p>
-                    My name's Sharon and I've been fixing cars since I was 8 years old. My uncle owned a shop back in Arizona and I would help him after school. Ever since my first tire change, I couldn't get enough. Your car's in good hands with me.
-                </p>
-                <p class="hidden">
-                    "Your car's in
-                    </br>
-                    good hands with me"
-                </p>
+                <h3>Oil Change</h3>
+                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/oilchange.png" alt="Oil Change" />
+
             </div>
             <div class="image col-sm-4">
-                <img class="imghover img-responsive" src="<?php bloginfo('template_directory'); ?>/img/midmech.png" alt="Logan" />
-                <h2>Logan</h2>
-                <p>
-                    My mom was a professional NASCAR racer so I've been around cars my whole life. I would often talk to the pit crew in between my mom's races and it's those interactions and conversations that's developed my appreciation for these fantastic creations. Your car's in good hands with me.
-                </p>
-                <p class="hidden">
-                    "Your car's in
-                    </br>
-                    good hands with me"
-                </p>
+                <h3>Brakes</h3>
+                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/brakes.png" alt="Brakes" />
+
             </div>
-            <div class="imghover image col-sm-4">
-                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/rightmech.png" alt="Luke" />
-                <h2>Luke</h2>
-                <p>
-                    My dad taught me everything I know about cars and every week since I was old enough to read, he would give me a new car manual to study. It was through these manuals that I grew to appreciate just how intricate our vehicles that we use everyday are and what sparked my interest in becoming a mechanic. You car's in good hands with me.
-                </p>
-                <p class="hidden">
-                    "Your car's in
-                    </br>
-                    good hands with me"
-                </p>
+            <div class="image col-sm-4">
+                <h3>Transmission</h3>
+                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/engine.png" alt="Engine" />
+
             </div>
 
         </div>
 
         <!-- 2nd Row of Pricing Images -->
 
-        <div class="row">
+        <div id="secondrow" class="row">
             <div class="image col-sm-4">
-                <img class="imghover img-responsive" src="<?php bloginfo('template_directory'); ?>/img/leftmech.png" alt="Sharon" />
-                <h2>Sharon</h2>
-                <p>
-                    My name's Sharon and I've been fixing cars since I was 8 years old. My uncle owned a shop back in Arizona and I would help him after school. Ever since my first tire change, I couldn't get enough. Your car's in good hands with me.
-                </p>
-                <p class="hidden">
-                    "Your car's in
-                    </br>
-                    good hands with me"
-                </p>
+                <h3>Tires</h3>
+                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/tire.png" alt="Tire" />
+
             </div>
             <div class="image col-sm-4">
-                <img class="imghover img-responsive" src="<?php bloginfo('template_directory'); ?>/img/midmech.png" alt="Logan" />
-                <h2>Logan</h2>
-                <p>
-                    My mom was a professional NASCAR racer so I've been around cars my whole life. I would often talk to the pit crew in between my mom's races and it's those interactions and conversations that's developed my appreciation for these fantastic creations. Your car's in good hands with me.
-                </p>
-                <p class="hidden">
-                    "Your car's in
-                    </br>
-                    good hands with me"
-                </p>
+                <h3>Air Conditioning</h3>
+                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/ac.png" alt="AC" />
+
             </div>
-            <div class="imghover image col-sm-4">
-                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/rightmech.png" alt="Luke" />
-                <h2>Luke</h2>
-                <p>
-                    My dad taught me everything I know about cars and every week since I was old enough to read, he would give me a new car manual to study. It was through these manuals that I grew to appreciate just how intricate our vehicles that we use everyday are and what sparked my interest in becoming a mechanic. You car's in good hands with me.
-                </p>
-                <p class="hidden">
-                    "Your car's in
-                    </br>
-                    good hands with me"
-                </p>
+            <div class="image col-sm-4">
+                <h3>Electrical</h3>
+                <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/img/electrical.png" alt="Electrical" />
+
             </div>
 
         </div>
